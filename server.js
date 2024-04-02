13d907ef358b13493b5b0b10bd47cf25f3e7b331
c4ei <his001_ccp@naver.com>
2024-04-02 21:22:57 +0900

    이 코드는 항상 가장 하단에 놓아야 잘됩니다.

--- server.js
+++ server.js
@@ -11,29 +11,14 @@ app.use(cors());
 
 
 app.use(express.static( path.join(__dirname, 'build') ))
+
 app.get('/', function(req,resp){
-  resp.sendFile( path.join(__dirname, '/build/index.html') )
+  resp.sendFile( path.join(__dirname, 'build/index.html') )
 }) 
-
-// app.use(express.static(path.join(__dirname, "FE_with_react/build")));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/FE_with_react/build/index.html"));
-// });
-
-
-// app.use( '/', express.static( path.join(__dirname, './client/build') ))
-
-// app.use( '/react', express.static( path.join(__dirname, 'react-project/build') ))
-
-// app.get('/react', function(req,resp){
-//   resp.sendFile( path.join(__dirname, 'react-project/build/index.html') )
-// })
-
 //이 코드는 항상 가장 하단에 놓아야 잘됩니다. 
-// app.get('*', function (req, resp) {
-//   resp.sendFile(path.join(__dirname, '/index.html'));
-// });
+app.get('*', function (req, resp) {
+  resp.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 
 app.listen(3013, function () {
