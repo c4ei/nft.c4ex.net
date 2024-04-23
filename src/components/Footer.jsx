459233358683cd288c9ad34f16d3b3e6c9e06550
c4ei <his001_ccp@naver.com>
2024-04-23 14:45:39 +0900

    mybe end?

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,9 +22,9 @@ const Footer = () => {
       {/* <p className="text-white text-right text-xs"> &copy;2024 All rights reserved </p> */}
       <p className="text-white text-base text-center cursor-pointer"><a href="/" className="mx-4 cursor-pointer">Market</a></p>
       <p className="text-white text-base text-center cursor-pointer"><a href="/collections" className="mx-4 cursor-pointer">Collection</a></p>
-      <p className="text-white text-base text-center cursor-pointer"><a href="https://c4ei.net/home" target="_blank" rel="noopener noreferrer">AAH</a></p>
-      <p className="text-white text-base text-center cursor-pointer"><a href="https://c4ex.net" target="_blank" rel="noopener noreferrer">AAH Buy</a></p>
-      <p className="text-white text-base text-center cursor-pointer"><a href="https://docs.google.com/document/d/1S3HcMW2EmzvCEtDeF4JdSrbZOuO0JdPOHl5Stc0lhbE/edit?usp=sharing" target="_blank" rel="noopener noreferrer">사용법(DOC)</a></p>
+      <p className="text-white text-base text-center cursor-pointer"><a href="https://c4ei.net/home" className="mx-4 cursor-pointer" target="_blank" rel="noopener noreferrer">AAH</a></p>
+      <p className="text-white text-base text-center cursor-pointer"><a href="https://c4ex.net" className="mx-4 cursor-pointer" target="_blank" rel="noopener noreferrer">AAH Buy</a></p>
+      <p className="text-white text-base text-center cursor-pointer"><a href="https://docs.google.com/document/d/1S3HcMW2EmzvCEtDeF4JdSrbZOuO0JdPOHl5Stc0lhbE/edit?usp=sharing" target="_blank" className="mx-4 cursor-pointer" rel="noopener noreferrer">사용법(DOC)</a></p>
     </div>
   )
 }
