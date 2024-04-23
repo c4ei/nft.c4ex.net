7bbfad62236ab29afac093ce5ef19419f62ac342
c4ei <his001_ccp@naver.com>
2024-04-23 14:40:49 +0900

    대충 1차완료?

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,15 +19,12 @@ const Footer = () => {
         <p className="text-white text-base text-center cursor-pointer"><a href="https://c4ex.net" target="_blank" rel="noopener noreferrer">Buy AAH</a></p> */}
       </div>
 
-      <p className="text-white text-right text-xs">
-        &copy;2024 All rights reserved
-        <br />
-        
-        화면에서 진행 경매가 보이지 않으면 우측의 Market 을 클릭해 주세요.
-        <br />If you do not see the ongoing auction on the screen, please click Market text (CTRL+F5).
-      </p>
+      {/* <p className="text-white text-right text-xs"> &copy;2024 All rights reserved </p> */}
       <p className="text-white text-base text-center cursor-pointer"><a href="/" className="mx-4 cursor-pointer">Market</a></p>
       <p className="text-white text-base text-center cursor-pointer"><a href="/collections" className="mx-4 cursor-pointer">Collection</a></p>
+      <p className="text-white text-base text-center cursor-pointer"><a href="https://c4ei.net/home" target="_blank" rel="noopener noreferrer">AAH</a></p>
+      <p className="text-white text-base text-center cursor-pointer"><a href="https://c4ex.net" target="_blank" rel="noopener noreferrer">AAH Buy</a></p>
+      <p className="text-white text-base text-center cursor-pointer"><a href="https://docs.google.com/document/d/1S3HcMW2EmzvCEtDeF4JdSrbZOuO0JdPOHl5Stc0lhbE/edit?usp=sharing" target="_blank" rel="noopener noreferrer">사용법(DOC)</a></p>
     </div>
   )
 }
